ddd97014d3879b4269a3242c5ee1d93a5b8a1980
Xiaoming Tu <xiaoming.tu@ranplan.co.uk>
2015-02-23 11:11:48 +0000

    Initial version of AutoHotKey.

---
+++ AutoHotKeyForm.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace AutoHotKey
+{
+    partial class AutoHotKeyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoHotKeyForm));
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lnkHomePage = new System.Windows.Forms.LinkLabel();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.ctxTrayIcon = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuRestore = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuStop = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.ctxTrayIcon.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSwitch.Location = new System.Drawing.Point(237, 3);
+            this.btnSwitch.Name = "btnSwitch";
+            this.tableLayoutPanel1.SetRowSpan(this.btnSwitch, 2);
+            this.btnSwitch.Size = new System.Drawing.Size(65, 53);
+            this.btnSwitch.TabIndex = 0;
+            this.btnSwitch.Text = "&Start";
+            this.btnSwitch.UseVisualStyleBackColor = true;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Keys:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Interval (ms):";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // txtKey
+            // 
+            this.txtKey.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtKey.Location = new System.Drawing.Point(120, 4);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(111, 20);
+            this.txtKey.TabIndex = 3;
+            this.txtKey.Text = "1";
+            this.txtKey.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInterval.Location = new System.Drawing.Point(120, 34);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(111, 20);
+            this.txtInterval.TabIndex = 4;
+            this.txtInterval.Text = "1000";
+            this.txtInterval.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 71F));
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnSwitch, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtKey, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtInterval, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(305, 59);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // lnkHomePage
+            // 
+            this.lnkHomePage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lnkHomePage.AutoSize = true;
+            this.lnkHomePage.Location = new System.Drawing.Point(12, 74);
+            this.lnkHomePage.Name = "lnkHomePage";
+            this.lnkHomePage.Size = new System.Drawing.Size(143, 13);
+            this.lnkHomePage.TabIndex = 6;
+            this.lnkHomePage.TabStop = true;
+            this.lnkHomePage.Text = "http://www.xiaomingtu.com/";
+            this.lnkHomePage.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkHomePage_LinkClicked);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Minimized to the system tray.";
+            this.notifyIcon1.BalloonTipTitle = "Auto Press Key";
+            this.notifyIcon1.ContextMenuStrip = this.ctxTrayIcon;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // ctxTrayIcon
+            // 
+            this.ctxTrayIcon.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuRestore,
+            this.toolStripSeparator2,
+            this.mnuStart,
+            this.mnuStop,
+            this.toolStripSeparator1,
+            this.mnuExit});
+            this.ctxTrayIcon.Name = "ctxTrayIcon";
+            this.ctxTrayIcon.Size = new System.Drawing.Size(140, 104);
+            this.ctxTrayIcon.Opening += new System.ComponentModel.CancelEventHandler(this.ctxTrayIcon_Opening);
+            // 
+            // mnuRestore
+            // 
+            this.mnuRestore.Name = "mnuRestore";
+            this.mnuRestore.Size = new System.Drawing.Size(139, 22);
+            this.mnuRestore.Text = "打开主界面";
+            this.mnuRestore.Click += new System.EventHandler(this.mnuRestore_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(136, 6);
+            // 
+            // mnuStart
+            // 
+            this.mnuStart.Name = "mnuStart";
+            this.mnuStart.Size = new System.Drawing.Size(139, 22);
+            this.mnuStart.Text = "开始(&S)";
+            this.mnuStart.Click += new System.EventHandler(this.mnuStart_Click);
+            // 
+            // mnuStop
+            // 
+            this.mnuStop.Name = "mnuStop";
+            this.mnuStop.Size = new System.Drawing.Size(139, 22);
+            this.mnuStop.Text = "停止(&T)";
+            this.mnuStop.Click += new System.EventHandler(this.mnuStop_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(136, 6);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(139, 22);
+            this.mnuExit.Text = "退出(X)";
+            this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
+            // 
+            // AutoHotKeyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 96);
+            this.Controls.Add(this.lnkHomePage);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "AutoHotKeyForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Press Keys";
+            this.Resize += new System.EventHandler(this.AutoHotKeyForm_Resize);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ctxTrayIcon.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSwitch;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.TextBox txtInterval;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel lnkHomePage;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip ctxTrayIcon;
+        private System.Windows.Forms.ToolStripMenuItem mnuStart;
+        private System.Windows.Forms.ToolStripMenuItem mnuStop;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.ToolStripMenuItem mnuRestore;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+    }
+}
+
---
+++ AutoHotKeyForm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Forms;
+using AutoHotKey.Properties;
+
+namespace AutoHotKey
+{
+    public partial class AutoHotKeyForm : Form
+    {
+        private bool _turnedOn;
+        private string _keys;
+
+        public AutoHotKeyForm()
+        {
+            InitializeComponent();
+        }
+
+        private bool TurnSwitch()
+        {
+            if (_turnedOn)  // stop it
+            {
+                btnSwitch.Text = Resources.Str_Start;
+                txtInterval.Enabled = true;
+                txtKey.Enabled = true;
+                _turnedOn = false;
+                timer1.Enabled = false;
+                return true;
+            }
+            else  // start it
+            {
+                int interval;
+                if (!Int32.TryParse(txtInterval.Text, out interval))
+                {
+                    MessageBox.Show(Resources.Err_InvalidInterval, Text,
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+                timer1.Interval = interval;
+                _keys = txtKey.Text;
+
+                btnSwitch.Text = Resources.Str_Stop;
+                txtInterval.Enabled = false;
+                txtKey.Enabled = false;
+                _turnedOn = true;
+                timer1.Enabled = true;
+                return true;
+            }
+        }
+
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            TurnSwitch();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_keys))
+                SendKeys.Send(_keys);
+        }
+
+        private void lnkHomePage_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://www.xiaomingtu.com/?ref=AutoHotKey");
+        }
+
+        private void mnuStart_Click(object sender, EventArgs e)
+        {
+            TurnSwitch();
+        }
+
+        private void mnuStop_Click(object sender, EventArgs e)
+        {
+            TurnSwitch();
+        }
+
+        private void mnuExit_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            Close();
+        }
+
+        private void ctxTrayIcon_Opening(object sender, CancelEventArgs e)
+        {
+            mnuStart.Checked = _turnedOn;
+            mnuStop.Checked = !_turnedOn;
+
+            mnuStart.Enabled = !_turnedOn;
+            mnuStop.Enabled = _turnedOn;
+        }
+
+        private void AutoHotKeyForm_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(500);
+                Hide();
+            }
+            else
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void mnuRestore_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+    }
+}
